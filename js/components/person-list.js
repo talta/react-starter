87a60da21f1c1c46970fd09fe36767d3d7b7faea
talta <t.taltallc@gmail.com>
2017-03-26 07:58:53 -0400

    proptypes added for the person component

--- js/components/person-list.js
+++ js/components/person-list.js
@@ -3,19 +3,35 @@ var ReactDOM = require('react-dom');
 
 var Person = require('./person');
 
-//////Rather than just being pure functions, stateful components are classes which contain a render method. 
-var PersonList = function() {
-    var people = [];
-    for (var i=0; i<5; i++) {
-        people.push(<Person />);
-    }
-    return (
-        <div className="person-list">
-            {people}
-        </div>
-    );
-};
+// //////Rather than just being pure functions, stateful components are classes which contain a render method. 
+// var PersonList = function() {
+//     var people = [];
+//     for (var i=0; i<5; i++) {
+//         people.push(<Person />);
+//     }
+//     return (
+//         <div className="person-list">
+//             {people}
+//         </div>
+//     );
+// };
 
+
+
+
+
+var PersonList = function(){
+	<div className = "PersonList">
+		<Person name = 'Derek Zoolander';
+    		imageUrl = 'http://mcgrelio.com/gallery/var/albums/vacanze/capodanno2004/capodanno2004_bluesteel/derek.jpg?m=1286923065';
+    		alt = `${name} picture`;
+    		job = 'Male model'/>
+    	<Person name='Donald Kruth'
+    		imageUrl = 'http://www-cs-faculty.stanford.edu/~uno/don.gif'
+    		alt='${name} picture'
+    		job= 'some guy' />
+    </div>
+}
 
 
 module.exports = PersonList;
--- js/components/person.js
+++ js/components/person.js
@@ -1,20 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Person = function() {
-    var name = 'Derek Zoolander';
-    var imageUrl = 'http://mcgrelio.com/gallery/var/albums/vacanze/capodanno2004/capodanno2004_bluesteel/derek.jpg?m=1286923065';
-    var alt = `${name} picture`;
-    var job = 'Male model';
+var Person = function(props) {
     return (
         <div className="person">
             <div className="person-name">{name}</div>
-            <img className="person-img" src={imageUrl} alt={alt} />
+            <img className="person-img" src={props.imageUrl} alt={props.alt} />
             <div className="person-job">
-                {job}
+                {props.job}
             </div>
         </div>
     );
 };
+
+
+
+Person.propTypes ={name : React.PropTypes.string.isRequired,
+					imageUrl: React.PropTypes.string,
+					alt: React.PropTypes.string,
+					job: React.PropTypes.string.required
+				 }
 
 module.exports = Person;
